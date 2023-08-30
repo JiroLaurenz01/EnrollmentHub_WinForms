169e79b0979f37733d2f872e561760f8f6999d40
Jiro Laurenz <jirolaurenz28@gmail.com>
2023-08-30 15:21:38 +0800

    INCLUDE COMMENTS FOR ALERT FORM

--- SchoolTracker/AlertForm.cs
+++ SchoolTracker/AlertForm.cs
@@ -14,6 +14,8 @@ namespace SchoolTracker
     {
         private int x, y;
 
+        // Defines a public enumeration named Action with three possible values: wait, start, and close.
+        // This enumeration represents different states of the alert form.
         public enum Action
         {
             wait,
@@ -21,6 +23,8 @@ namespace SchoolTracker
             close
         }
 
+        // Defines a public enumeration named Type with four possible values: Success, Warning, Error, and Info.
+        // This enumeration represents different types of alerts.
         public enum Type
         {
             Success,
@@ -31,7 +35,6 @@ namespace SchoolTracker
 
         private AlertForm.Action action;
 
-
         public AlertForm()
         {
             InitializeComponent();
@@ -39,16 +42,21 @@ namespace SchoolTracker
 
         private void timer_Tick(object sender, EventArgs e)
         {
+            // Event handler for the Timer's tick event.
+
             switch (action)
             {
                 case Action.wait:
+                    // If the action is "wait", set the timer interval to 5 seconds and change the action to "close".
                     timer.Interval = 5000;
                     action = Action.close;
                     break;
                 case Action.start:
+                    // If the action is "start", set the timer interval to 1 millisecond and gradually increase opacity.
                     timer.Interval = 1;
                     Opacity += 0.1;
 
+                    // Move the form to the left until its X coordinate matches the target X coordinate.
                     if (x < Location.X)
                         Left--;
                     else
@@ -56,9 +64,11 @@ namespace SchoolTracker
                             action = Action.wait;
                     break;
                 case Action.close:
+                    // If the action is "close", set the timer interval to 1 millisecond and gradually decrease opacity.
                     timer.Interval = 1;
                     Opacity -= 0.1;
 
+                    // Move the form to the left and close it when opacity reaches 0.
                     Left -= 3;
                     if (base.Opacity == 0.0)
                         base.Close();
@@ -68,35 +78,42 @@ namespace SchoolTracker
 
         private void removeBtn_Click(object sender, EventArgs e)
         {
+            // Event handler for the remove button's click event.
+
             timer.Interval = 1;
             action = Action.close;
         }
 
         public void ShowAlert(string msg, Type type)
         {
+            // Method to display an alert message with a specified type.
+
             Opacity = 0.0;
             StartPosition = FormStartPosition.Manual;
             string fname;
 
+            // Loop to find an available form name.
             for (int i = 1; i < 10; i++)
             {
                 fname = "alert" + i.ToString();
                 AlertForm form = (AlertForm)Application.OpenForms[fname];
 
+                // If no form with the current name exists, use this name for the form.
                 if (form == null)
                 {
                     Name = fname;
-                    x = Screen.PrimaryScreen.WorkingArea.Width - Width + 15;
-                    y = Screen.PrimaryScreen.WorkingArea.Height - Height * i - 5 * i;
-                    Location = new Point(this.x, this.y);
+                    x = Screen.PrimaryScreen.WorkingArea.Width - Width + 15; // Calculate the X coordinate.
+                    y = Screen.PrimaryScreen.WorkingArea.Height - Height * i - 5 * i; // Calculate the Y coordinate.
+                    Location = new Point(this.x, this.y); // Set the form's location.
 
                     break;
                 }
 
             }
 
-            x = Screen.PrimaryScreen.WorkingArea.Width - base.Width - 5;
+            x = Screen.PrimaryScreen.WorkingArea.Width - base.Width - 5; // Calculate the X coordinate for the form.
 
+            // Set the form's appearance based on the specified alert type.
             switch (type)
             {
                 case Type.Success:
@@ -118,10 +135,10 @@ namespace SchoolTracker
             }
 
 
-            labelMessage.Text = msg;
+            labelMessage.Text = msg; // Set the alert message.
 
             Show();
-            action = Action.start;
+            action = Action.start; // Set the action to "start" for opening animation.
             timer.Interval = 1;
             timer.Start();
         }
