9b2dc3f32351e547c226cba19303d61d8aecb455
Jiro Laurenz <jirolaurenz28@gmail.com>
2023-08-30 15:15:06 +0800

    ADD A FUNCTION TO DISPLAY ALERT

--- SchoolTracker/Functionality.cs
+++ SchoolTracker/Functionality.cs
@@ -77,5 +77,17 @@ namespace SchoolTracker
 
         #endregion
 
+        #region FUNCTION TO DISPLAY ALERT
+
+        // This method is used to display an alert message using a custom AlertForm.
+        public void Alert(string msg, AlertForm.Type type)
+        {
+            AlertForm alertForm = new AlertForm();
+
+            // Call the ShowAlert method of the AlertForm instance, passing the message and alert type as parameters.
+            alertForm.ShowAlert(msg, type);
+        }
+
+        #endregion
     }
 }
