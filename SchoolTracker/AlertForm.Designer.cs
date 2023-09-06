980c27815d11ce19817dec245a3793d010503023
Jiro Laurenz <jirolaurenz28@gmail.com>
2023-09-06 18:47:51 +0800

    MINOR UPDATE FOR THE DESIGN OF ALERT FORM

--- SchoolTracker/AlertForm.Designer.cs
+++ SchoolTracker/AlertForm.Designer.cs
@@ -31,10 +31,11 @@
             this.components = new System.ComponentModel.Container();
             this.labelMessage = new System.Windows.Forms.Label();
             this.timer = new System.Windows.Forms.Timer(this.components);
-            this.removeBtn = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.removeBtn)).BeginInit();
+            this.guna2ContainerControl1 = new Guna.UI2.WinForms.Guna2ContainerControl();
+            this.guna2ControlBox1 = new Guna.UI2.WinForms.Guna2ControlBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.guna2ContainerControl1.SuspendLayout();
             this.SuspendLayout();
             // 
             // labelMessage
@@ -42,7 +43,7 @@
             this.labelMessage.AutoSize = true;
             this.labelMessage.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelMessage.ForeColor = System.Drawing.Color.White;
-            this.labelMessage.Location = new System.Drawing.Point(63, 21);
+            this.labelMessage.Location = new System.Drawing.Point(59, 46);
             this.labelMessage.Name = "labelMessage";
             this.labelMessage.Size = new System.Drawing.Size(116, 21);
             this.labelMessage.TabIndex = 0;
@@ -52,43 +53,57 @@
             // 
             this.timer.Tick += new System.EventHandler(this.timer_Tick);
             // 
-            // removeBtn
-            // 
-            this.removeBtn.Image = global::SchoolTracker.Properties.Resources.icons8_cancel_25px;
-            this.removeBtn.Location = new System.Drawing.Point(330, 16);
-            this.removeBtn.Name = "removeBtn";
-            this.removeBtn.Size = new System.Drawing.Size(26, 30);
-            this.removeBtn.TabIndex = 3;
-            this.removeBtn.TabStop = false;
-            this.removeBtn.Click += new System.EventHandler(this.removeBtn_Click);
-            // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::SchoolTracker.Properties.Resources.success;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 37);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(41, 39);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
+            // guna2ContainerControl1
+            // 
+            this.guna2ContainerControl1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2ContainerControl1.Controls.Add(this.guna2ControlBox1);
+            this.guna2ContainerControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.guna2ContainerControl1.FillColor = System.Drawing.SystemColors.HotTrack;
+            this.guna2ContainerControl1.Location = new System.Drawing.Point(0, 0);
+            this.guna2ContainerControl1.Name = "guna2ContainerControl1";
+            this.guna2ContainerControl1.Size = new System.Drawing.Size(367, 23);
+            this.guna2ContainerControl1.TabIndex = 4;
+            this.guna2ContainerControl1.Text = "guna2ContainerControl1";
+            // 
+            // guna2ControlBox1
+            // 
+            this.guna2ControlBox1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.guna2ControlBox1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.guna2ControlBox1.FillColor = System.Drawing.Color.Transparent;
+            this.guna2ControlBox1.IconColor = System.Drawing.Color.White;
+            this.guna2ControlBox1.Location = new System.Drawing.Point(328, 0);
+            this.guna2ControlBox1.Name = "guna2ControlBox1";
+            this.guna2ControlBox1.Size = new System.Drawing.Size(39, 23);
+            this.guna2ControlBox1.TabIndex = 0;
+            // 
             // AlertForm
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.SystemColors.Highlight;
-            this.ClientSize = new System.Drawing.Size(367, 62);
-            this.Controls.Add(this.removeBtn);
+            this.ClientSize = new System.Drawing.Size(367, 96);
+            this.Controls.Add(this.guna2ContainerControl1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.labelMessage);
             this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "AlertForm";
+            this.Opacity = 0.7D;
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.Text = "Form_Alert";
             this.TopMost = true;
-            ((System.ComponentModel.ISupportInitialize)(this.removeBtn)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.guna2ContainerControl1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -99,6 +114,7 @@
         private System.Windows.Forms.Label labelMessage;
         private System.Windows.Forms.PictureBox pictureBox1;
         internal System.Windows.Forms.Timer timer;
-        private System.Windows.Forms.PictureBox removeBtn;
+        private Guna.UI2.WinForms.Guna2ContainerControl guna2ContainerControl1;
+        private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox1;
     }
 }
