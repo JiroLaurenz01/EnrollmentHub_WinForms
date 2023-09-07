0c14b41dbd6e39ec694a40e51500610aec8250ac
Jiro Laurenz <jirolaurenz28@gmail.com>
2023-09-07 13:13:40 +0800

    NOTICE LABEL FOR ALERT FORM AND ADJUST THE INTERVAL FROM 5 SECONDS INTO 2.5 SECONDS

--- SchoolTracker/AlertForm.Designer.cs
+++ SchoolTracker/AlertForm.Designer.cs
@@ -35,6 +35,7 @@
             this.containerControl = new Guna.UI2.WinForms.Guna2ContainerControl();
             this.guna2ControlBox1 = new Guna.UI2.WinForms.Guna2ControlBox();
             this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.noticeLabel = new Guna.UI2.WinForms.Guna2HtmlLabel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.containerControl.SuspendLayout();
             this.SuspendLayout();
@@ -42,11 +43,11 @@
             // labelMessage
             // 
             this.labelMessage.AutoSize = true;
-            this.labelMessage.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMessage.Font = new System.Drawing.Font("Bahnschrift", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelMessage.ForeColor = System.Drawing.Color.White;
-            this.labelMessage.Location = new System.Drawing.Point(59, 46);
+            this.labelMessage.Location = new System.Drawing.Point(59, 50);
             this.labelMessage.Name = "labelMessage";
-            this.labelMessage.Size = new System.Drawing.Size(116, 21);
+            this.labelMessage.Size = new System.Drawing.Size(124, 23);
             this.labelMessage.TabIndex = 0;
             this.labelMessage.Text = "Message Text";
             // 
@@ -57,7 +58,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::SchoolTracker.Properties.Resources.success;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 37);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 43);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(41, 39);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -67,12 +68,13 @@
             // containerControl
             // 
             this.containerControl.BackColor = System.Drawing.Color.Transparent;
+            this.containerControl.Controls.Add(this.noticeLabel);
             this.containerControl.Controls.Add(this.guna2ControlBox1);
             this.containerControl.Dock = System.Windows.Forms.DockStyle.Top;
             this.containerControl.FillColor = System.Drawing.Color.DarkOrange;
             this.containerControl.Location = new System.Drawing.Point(0, 0);
             this.containerControl.Name = "containerControl";
-            this.containerControl.Size = new System.Drawing.Size(367, 23);
+            this.containerControl.Size = new System.Drawing.Size(367, 31);
             this.containerControl.TabIndex = 4;
             this.containerControl.Text = "guna2ContainerControl1";
             // 
@@ -84,18 +86,29 @@
             this.guna2ControlBox1.IconColor = System.Drawing.Color.White;
             this.guna2ControlBox1.Location = new System.Drawing.Point(328, 0);
             this.guna2ControlBox1.Name = "guna2ControlBox1";
-            this.guna2ControlBox1.Size = new System.Drawing.Size(39, 23);
+            this.guna2ControlBox1.Size = new System.Drawing.Size(39, 31);
             this.guna2ControlBox1.TabIndex = 0;
             // 
             // guna2Elipse1
             // 
             this.guna2Elipse1.TargetControl = this;
             // 
+            // noticeLabel
+            // 
+            this.noticeLabel.BackColor = System.Drawing.Color.Transparent;
+            this.noticeLabel.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.noticeLabel.ForeColor = System.Drawing.Color.White;
+            this.noticeLabel.Location = new System.Drawing.Point(14, 4);
+            this.noticeLabel.Name = "noticeLabel";
+            this.noticeLabel.Size = new System.Drawing.Size(49, 21);
+            this.noticeLabel.TabIndex = 1;
+            this.noticeLabel.Text = "Notice";
+            // 
             // AlertForm
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.Orange;
-            this.ClientSize = new System.Drawing.Size(367, 96);
+            this.ClientSize = new System.Drawing.Size(367, 101);
             this.Controls.Add(this.containerControl);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.labelMessage);
@@ -109,6 +122,7 @@
             this.TopMost = true;
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.containerControl.ResumeLayout(false);
+            this.containerControl.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -122,5 +136,6 @@
         private Guna.UI2.WinForms.Guna2ContainerControl containerControl;
         private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox1;
         private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
+        private Guna.UI2.WinForms.Guna2HtmlLabel noticeLabel;
     }
 }
